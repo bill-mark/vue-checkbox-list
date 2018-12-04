ec9eba0d767f877cc43db04658efb1d258403d1e
bill.mark <2286449127@qq.com>
2018-12-04 18:55:01 +0800

    up

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,6 @@
 import vueCheckboxList from './vue-checkbox-list.vue'
 
-if (typeof window !== 'undefined' && window.Vue) { window.Vue.use(paykeyboard); }
+if (typeof window !== 'undefined' && window.Vue) { window.Vue.use(vueCheckboxList); }
 
 const  CheckboxList = {
 	install:function (Vue,options) {
